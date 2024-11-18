4878f64824ee1a65d59e8508debfb895b2e6a274
Aluno <Aluno@LS05M01>
2024-11-18 13:42:32 -0300

    Add project files.

---
+++ Ecommerce/Entidade/ProdutosEntidade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Entidade
+{
+    public class ProdutosEntidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public float Preco { get; set; }
+        public int Estoque { get; set; }
+
+        public object[] Linha()
+        {
+            return new object[] { Id, Nome, Descricao, Preco, Estoque };
+        }
+    }
+}
---
+++ Ecommerce/Entidade/VendasEntidade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Entidade
+{
+    public class VendasEntidade
+    {
+        public int Id { get; set; }
+        public DateTime DataVenda { get; set; }
+        public float Total { get; set; }
+
+        public object[] Linha()
+        {
+            return new object[] { Id, DataVenda, Total };
+        }
+    }
+}
