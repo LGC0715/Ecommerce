82394bc53d926ff29722b910e48b65af64fd4517
LuSanchess <luisa.silva@anglobauru.com.br>
2024-11-20 10:56:33 -0300

    FORMS PROD MASTER

--- Ecommerce/Formularios/FrmProdutos.Designer.cs
+++ Ecommerce/Formularios/FrmProdutos.Designer.cs
@@ -29,81 +29,197 @@ namespace Ecommerce.Formularios
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnadd_produto = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.txtpesquisa = new System.Windows.Forms.TextBox();
-            this.dtGridProdutos = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dtGridProdutos)).BeginInit();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtprecoprod = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.Label();
+            this.numId = new System.Windows.Forms.NumericUpDown();
+            this.txtPesquisar = new System.Windows.Forms.Label();
+            this.txtPreco = new System.Windows.Forms.Label();
+            this.txtNomeprod = new System.Windows.Forms.TextBox();
+            this.dtprodutos = new System.Windows.Forms.DataGridView();
+            this.txtProduto = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numId)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtprodutos)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnadd_produto
-            // 
-            this.btnadd_produto.Location = new System.Drawing.Point(33, 16);
-            this.btnadd_produto.Margin = new System.Windows.Forms.Padding(4);
-            this.btnadd_produto.Name = "btnadd_produto";
-            this.btnadd_produto.Size = new System.Drawing.Size(224, 28);
-            this.btnadd_produto.TabIndex = 19;
-            this.btnadd_produto.Text = "Cadastrar Produtos";
-            this.btnadd_produto.UseVisualStyleBackColor = true;
-            this.btnadd_produto.Click += new System.EventHandler(this.btnadd_disc_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(631, -16);
+            this.label4.Location = new System.Drawing.Point(473, -13);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(68, 16);
+            this.label4.Size = new System.Drawing.Size(53, 13);
             this.label4.TabIndex = 18;
             this.label4.Text = "Pesquisar";
             // 
-            // txtpesquisa
+            // btnLimpar
             // 
-            this.txtpesquisa.Location = new System.Drawing.Point(633, 22);
-            this.txtpesquisa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtpesquisa.Name = "txtpesquisa";
-            this.txtpesquisa.Size = new System.Drawing.Size(395, 22);
-            this.txtpesquisa.TabIndex = 17;
-            this.txtpesquisa.TextChanged += new System.EventHandler(this.txtpesquisa_TextChanged);
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(491, 31);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 32;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
             // 
-            // dtGridProdutos
+            // btnSalvar
             // 
-            this.dtGridProdutos.AllowUserToAddRows = false;
-            this.dtGridProdutos.AllowUserToDeleteRows = false;
-            this.dtGridProdutos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dtGridProdutos.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
-            this.dtGridProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtGridProdutos.Location = new System.Drawing.Point(20, 79);
-            this.dtGridProdutos.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.dtGridProdutos.Name = "dtGridProdutos";
-            this.dtGridProdutos.ReadOnly = true;
-            this.dtGridProdutos.RowHeadersWidth = 51;
-            this.dtGridProdutos.Size = new System.Drawing.Size(1027, 492);
-            this.dtGridProdutos.TabIndex = 16;
-            this.dtGridProdutos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtGridProdutos_CellDoubleClick);
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(410, 31);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 31;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox1.Location = new System.Drawing.Point(282, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 30;
+            // 
+            // txtprecoprod
+            // 
+            this.txtprecoprod.AutoSize = true;
+            this.txtprecoprod.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtprecoprod.Location = new System.Drawing.Point(279, 15);
+            this.txtprecoprod.Name = "txtprecoprod";
+            this.txtprecoprod.Size = new System.Drawing.Size(43, 16);
+            this.txtprecoprod.TabIndex = 29;
+            this.txtprecoprod.Text = "Preço";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtName.Location = new System.Drawing.Point(118, 33);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(158, 20);
+            this.txtName.TabIndex = 28;
+            // 
+            // txtNome
+            // 
+            this.txtNome.AutoSize = true;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(115, 14);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(47, 16);
+            this.txtNome.TabIndex = 27;
+            this.txtNome.Text = "Nome ";
+            // 
+            // txtId
+            // 
+            this.txtId.AutoSize = true;
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(22, 14);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(20, 16);
+            this.txtId.TabIndex = 26;
+            this.txtId.Text = "ID";
+            // 
+            // numId
+            // 
+            this.numId.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.numId.Location = new System.Drawing.Point(25, 33);
+            this.numId.Name = "numId";
+            this.numId.Size = new System.Drawing.Size(87, 20);
+            this.numId.TabIndex = 25;
+            // 
+            // txtPesquisar
+            // 
+            this.txtPesquisar.AutoSize = true;
+            this.txtPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPesquisar.Location = new System.Drawing.Point(22, 73);
+            this.txtPesquisar.Name = "txtPesquisar";
+            this.txtPesquisar.Size = new System.Drawing.Size(68, 16);
+            this.txtPesquisar.TabIndex = 24;
+            this.txtPesquisar.Text = "Pesquisar";
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.AutoSize = true;
+            this.txtPreco.Font = new System.Drawing.Font("Alef", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPreco.Location = new System.Drawing.Point(131, 30);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(0, 22);
+            this.txtPreco.TabIndex = 23;
+            // 
+            // txtNomeprod
+            // 
+            this.txtNomeprod.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtNomeprod.Location = new System.Drawing.Point(96, 72);
+            this.txtNomeprod.Name = "txtNomeprod";
+            this.txtNomeprod.Size = new System.Drawing.Size(259, 20);
+            this.txtNomeprod.TabIndex = 22;
+            // 
+            // dtprodutos
+            // 
+            this.dtprodutos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtprodutos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dtprodutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtprodutos.Location = new System.Drawing.Point(12, 98);
+            this.dtprodutos.Name = "dtprodutos";
+            this.dtprodutos.Size = new System.Drawing.Size(589, 268);
+            this.dtprodutos.TabIndex = 21;
+            // 
+            // txtProduto
+            // 
+            this.txtProduto.AutoSize = true;
+            this.txtProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProduto.Location = new System.Drawing.Point(21, 41);
+            this.txtProduto.Name = "txtProduto";
+            this.txtProduto.Size = new System.Drawing.Size(0, 20);
+            this.txtProduto.TabIndex = 20;
             // 
             // FrmProdutos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 601);
-            this.Controls.Add(this.btnadd_produto);
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(607, 376);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtprecoprod);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.numId);
+            this.Controls.Add(this.txtPesquisar);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.txtNomeprod);
+            this.Controls.Add(this.dtprodutos);
+            this.Controls.Add(this.txtProduto);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.txtpesquisa);
-            this.Controls.Add(this.dtGridProdutos);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmProdutos";
             this.Text = "FormProdutos";
-            ((System.ComponentModel.ISupportInitialize)(this.dtGridProdutos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numId)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtprodutos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnadd_produto;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtpesquisa;
-        private System.Windows.Forms.DataGridView dtGridProdutos;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txtprecoprod;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label txtNome;
+        private System.Windows.Forms.Label txtId;
+        private System.Windows.Forms.NumericUpDown numId;
+        private System.Windows.Forms.Label txtPesquisar;
+        private System.Windows.Forms.Label txtPreco;
+        private System.Windows.Forms.TextBox txtNomeprod;
+        private System.Windows.Forms.DataGridView dtprodutos;
+        private System.Windows.Forms.Label txtProduto;
     }
 }
